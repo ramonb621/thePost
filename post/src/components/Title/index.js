f4aedad75487434f04897485c5af0843731a5fa6
Ramon Barajas <ramonb621@yahoo.com>
2020-12-20 18:23:53 -0800

    added pages folder with Home and Profile subfolders (including start-up files) and components folder (with Title subfolder. Beginning to work on front-end styling and layout of application.

---
+++ post/src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+    return (
+        <p className="title">Art Post</p>
+    );
+}
+
+export default Title;
---
+++ post/src/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from "react";
+
+class Home extends Component {
+    
+    componentDidMount() {
+
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        }); 
+    };
+
+    render() {
+        return (
+            <div className="container">
+            <p className="main-heading">Home</p>
+            </div>
+        )
+    }
+}
+export default Home;
